698a9b8890ee9572cc4b3b401d7c901679c2964f
FelipeRoxas <feliperozas5971@gmail.com>
2025-05-05 11:42:18 -0400

    commit 2
    parte 2 del ejemplo

--- assets/js/scriptindex.js
+++ assets/js/scriptindex.js
@@ -1,14 +1,20 @@
 function validar(){
     let eNombre = document.getElementById("nombre")
     let vNombre = eNombre.value
+    let eErrorNombre = document.getElementById("errorNombre")
     console.log(eNombre)
     console.log(vNombre)
     console.log(vNombre.length)
-    if (vNombre.length > 3){
+    if (vNombre.length < 3){
         console.log("pasa validacion, guarda en la bd")
+        alert("el nombre debe tener al menos 3 caracteres")
+        eErrorNombre.innerText = "el nombre debe tener al menos 3 caracteres"
+        eNombre.style.backgroundColor = "Red"
     }
     else{
-    alert("el nombre debe tener al menos 3 caracteres")
+    console.log("Todo bien")
+    eErrorNombre.innerText = ""
+    eNombre.style.backgroundColor = "green"
     }
 }
 // def validar():
