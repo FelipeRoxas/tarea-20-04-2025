d8831def8a4e333b2f87e54ffd03c9d3868104ce
FelipeRoxas <feliperozas5971@gmail.com>
2025-05-05 12:12:17 -0400

    punto de control 3
    ahora el boton funciona con las 2 casillas

--- assets/js/scriptindex.js
+++ assets/js/scriptindex.js
@@ -2,20 +2,37 @@ function validar(){
     let eNombre = document.getElementById("nombre")
     let vNombre = eNombre.value
     let eErrorNombre = document.getElementById("errorNombre")
-    console.log(eNombre)
-    console.log(vNombre)
-    console.log(vNombre.length)
+    let eApellido = document.getElementById("apellido")
+    let vApellido = eApellido.value
+    let eErrorApellido = document.getElementById("errorApellido")
     if (vNombre.length < 3){
-        console.log("pasa validacion, guarda en la bd")
-        alert("el nombre debe tener al menos 3 caracteres")
+        console.log("pocos caracteres")
         eErrorNombre.innerText = "el nombre debe tener al menos 3 caracteres"
-        eNombre.style.backgroundColor = "Red"
+        eNombre.style.backgroundColor = "red"
+        eNombre.style.color = "white"
     }
     else{
     console.log("Todo bien")
     eErrorNombre.innerText = ""
     eNombre.style.backgroundColor = "green"
     }
+    if (vApellido.length < 3){
+        console.log("pocos caracteres")
+        eErrorApellido.innerText = "el nombre debe tener al menos 3 caracteres"
+        eApellido.style.background = "red"
+        eApellido.style.color = "yellow"
+    }
+    else{
+        console.log("Todo Correcto")
+        eErrorApellido.innerText = ""
+        eApellido.style.backgroundColor = "green"
+    }
+    if (vNombre.length < 3 && vApellido.length < 3){
+        alert("el nombre debe tener al menos 3 caracteres")
+
+    }
+    
 }
+
 // def validar():
 //     asdasd
