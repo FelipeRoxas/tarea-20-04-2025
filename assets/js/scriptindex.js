bce54e82831f69c2ab7c1ea93fe02407042f93c8
FelipeRoxas <feliperozas5971@gmail.com>
2025-05-12 12:30:52 -0400

    commit 1 dia12 mayo
    commit inicio dia 12

--- assets/js/scriptindex.js
+++ assets/js/scriptindex.js
@@ -1,3 +1,4 @@
+let personas = []
 function validar(){
     let eNombre = document.getElementById("nombre")
     let vNombre = eNombre.value
@@ -5,8 +6,19 @@ function validar(){
     let eApellido = document.getElementById("apellido")
     let vApellido = eApellido.value
     let eErrorApellido = document.getElementById("errorApellido")
-    validarLargoMinimo(eNombre,vNombre,eErrorNombre)
-    validarLargoMinimo(eApellido,vApellido,eErrorApellido)
+    let fnombre =  validarLargoMinimo(eNombre,vNombre,eErrorNombre)
+    let fapellido = validarLargoMinimo(eApellido,vApellido,eErrorApellido)
+    if(fnombre == "exito" && fapellido == "exito"){
+        let p = {
+            nombre:vNombre,
+            apellido:vApellido
+        }
+        personas.push(p)
+        console.log(personas)
+        eNombre.value = ""
+        eApellido.value = ""
+        cargarDatos()
+    }
 
     
 }
@@ -18,12 +30,27 @@ function validarLargoMinimo(elemento,valor,eError){
         eError.innerText = "debes ingresar mas de 3 caracteres"
         elemento.style.backgroundColor = "red"
         elemento.style.color = "white"
+        return "falla"
     }else{
         console.log("Todo bien")
         eError.innerText = ""
         eError.style.backgroundColor = "green"
+        return "exito"
     }
 }
+function cargarDatos(){
+    console.log("Cargandooooo.....")
+    let mapPersonas = personas.map((p,index)=>{
+        return "<tr><td>"+p.nombre+
+                "</td></th>"+p.apellido+
+                "</td><tr><button type='button' value='"+index+"'>Eliminar</button></td></tr"
+    })
+    let tablaPersonas = document.getElementById("tablaPersonas")
+    let strTablaPersonas = mapPersonas.join("")
+    tablaPersonas.innerHTML = strTablaPersonas
+    console.log(mapPersonas)
+}
+
 
 // def validar():
 //     asdasd
